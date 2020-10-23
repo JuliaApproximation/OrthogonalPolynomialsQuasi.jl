7696a789afb441bdfab2d78a4316987812f5bf90
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2020-10-23 13:55:49 +0100

    Update test_jacobi.jl

--- test/test_jacobi.jl
+++ test/test_jacobi.jl
@@ -5,6 +5,8 @@ import OrthogonalPolynomialsQuasi: recurrencecoefficients, basis
     @testset "promotion and conversion" begin
         A,B=promote(Jacobi{Int64}(1,1),Legendre{Float64}())
         @test eltype(A) == Float64
+        @test Jacobi{Int64}(0,0) == Legendre{Float64}()
+        @test Jacobi{Int64}(0,0)\Jacobi{Int64}(0,0) == Jacobi{Int64}(0,0)\Legendre{Float64}() == Legendre{Float64}()\Legendre{Float64}()
     end
     @testset "JacobiWeight" begin
         a,b = 0.1,0.2
